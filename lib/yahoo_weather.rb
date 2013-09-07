81343db289868c2558358c04b174ddb0df3d495b
Ildar Manzhikov <manzhikov@gmail.com>
2013-09-07 20:49:47 +0400

    Fix issue #1: Doesn't work with Rails 4

--- lib/yahoo_weather.rb
+++ lib/yahoo_weather.rb
@@ -1,6 +1,12 @@
 require 'nokogiri'
 require 'open-uri'
 
+module YahooWeather
+  def self.code_string(id)
+    I18n.t id, scope: [:yahoo_weather, :code]
+  end
+end
+
 require 'yahoo_weather/client'
 require 'yahoo_weather/response'
 require 'yahoo_weather/wind'
@@ -10,9 +16,3 @@ require 'yahoo_weather/condition'
 require 'yahoo_weather/forecast'
 require 'yahoo_weather/location'
 require 'yahoo_weather/units'
-
-module YahooWeather
-  def self.code_string(id)
-    I18n.t id, scope: [:yahoo_weather, :code]
-  end
-end
